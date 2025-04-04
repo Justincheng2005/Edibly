0d6ae94c06365932dd656b0cc0eb5c5ad25b575d
OmarHOsman <oosman@umass.edu>
2025-04-04 16:33:11 -0400

    Adjusted Backend Structure (added controller and route files

--- server/index.js
+++ server/index.js
@@ -6,11 +6,20 @@ const cors = require("cors")
 app.use(cors())
 app.use(express.json())
 
+const userRoutes = require('./routes/userRoutes')
+const mealRoutes = require('./routes/mealRoutes')
+const diningRoutes = require('./routes/diningRoutes')
+
+app.use('/users', userRoutes)
+app.use('/meals', mealRoutes)
+app.use('/diningHalls', diningRoutes)
+
 //basic route
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
-  
-app.listen(5000, () => {
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log(`Server running on port 5000`);
 });
