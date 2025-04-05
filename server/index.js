fd1c842e4f60f6c032cedc20ea8881907af3da7f
OmarHOsman <oosman@umass.edu>
2025-04-05 18:48:00 -0400

    Reworked file structure to reflect justin's changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-import { query } from "./db.js"; 
+import { query } from "./db/db";
 
 dotenv.config();
 const app = express();
