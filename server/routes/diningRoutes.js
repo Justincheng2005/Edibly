6ad8f8c740e0a65ecff67a2addce70845006335e
Justin Cheng <justincheng2005@gmail.com>
2025-04-05 22:57:17 -0400

    added example route and fixed some syntax issues in backend

---
+++ server/routes/diningRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+// import{
+//     //functions,
+// } from './controllers/diningCont.js';
+
+
+const router = Router();
+
+// router.get('/', doSomethingController as any)
+// will return the result of your doSomethingController
+
+export default router;
