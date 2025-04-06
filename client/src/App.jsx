f5ebdc6303f4ad59d39b6340484f146268b8b4a4
hdrahman <haamed1.450@gmail.com>
2025-04-06 19:33:48 -0400

    fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DiningHalls from './pages/DiningHalls'
+import HomePage from './pages/HomePage'
 function App() {
 
   return (
-      <Router>
+    <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/dining-halls" element={<DiningHalls />} />
-        {}
+        { }
       </Routes>
-      </Router>
+    </Router>
   )
 }
 
