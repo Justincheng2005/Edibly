0bfcdae63cf67ea5f6a9f46e47bf511488a473b6
hdrahman <haamed1.450@gmail.com>
2025-04-07 15:44:59 -0400

    database fetching stuff

--- client/src/components/DHallCard.jsx
+++ client/src/components/DHallCard.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './DHallCard.css';
 import defaultImage from '../images/hampshire.jpg';
+// Import all the images we'll need
+import hampshireImg from '../images/hampshire.jpg';
+import berkshireImg from '../images/berkshire.jpg';
+import valentineImg from '../images/valentine.jpg';
 
 const DHallCard = ({ diningHall }) => {
+  // Map of available images
+  const localImages = {
+    'hampshire.jpg': hampshireImg,
+    'berkshire.jpg': berkshireImg,
+    'valentine.jpg': valentineImg,
+    // Add other images as needed
+  };
+
+  // Determine image source based on database value
+  const getImageSrc = () => {
+    if (!diningHall.image) return defaultImage;
+
+    // Check if the image is a full URL
+    if (diningHall.image.startsWith('http')) {
+      return diningHall.image;
+    }
+
+    // Check if we have the image in our local map
+    if (localImages[diningHall.image]) {
+      return localImages[diningHall.image];
+    }
+
+    // Fallback to default
+    return defaultImage;
+  };
+
   const handleImageError = (e) => {
     e.target.src = defaultImage;
   };
@@ -17,7 +47,7 @@ const DHallCard = ({ diningHall }) => {
       >
         <div className="dhall-image-wrapper">
           <img
-            src={diningHall.image || defaultImage}
+            src={getImageSrc()}
             alt={diningHall.name}
             className="dhall-image"
             onError={handleImageError}
