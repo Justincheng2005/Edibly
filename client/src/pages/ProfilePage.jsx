f37b516b39d7398d6f149641a7a90b8842234dfa
OmarHOsman <oosman@umass.edu>
2025-04-13 14:51:22 -0400

    General Code Restructuring & Initialized Profile Frontend

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,17 @@
+import MainHeader from "../components/MainHeader";
+import Navbar from "../components/Navbar";
+
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <MainHeader/>
+            <Navbar/>
+            <header>
+                <h2>Testing page rn</h2>
+            </header>
+        </div>
+    );
+}
+
+export default ProfilePage
